e0e9b2eeafee484d774998266bce9535b8cc5b7e
Isaac Alencar <alencarisaac3@gmail.com>
2023-10-23 11:17:29 -0300

    set countdown to restore every iteration

--- src/hooks/useCountdown.ts
+++ src/hooks/useCountdown.ts
@@ -1,8 +1,12 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+import { useGoal } from "@/providers/GoalProvider";
 
 export const useCountdown = () => {
+  const { completeIterations, currentIteration, totalIterations } = useGoal();
+
   const HOUR_IN_MINUTES = 60;
-  const DEFAULT_TIMER_VALUE = HOUR_IN_MINUTES;
+  const DEFAULT_TIMER_VALUE = 60;
 
   const intervalRef = useRef<NodeJS.Timeout>();
   const [timer, setTimer] = useState<number>(DEFAULT_TIMER_VALUE);
@@ -11,14 +15,16 @@ export const useCountdown = () => {
   const hour = Math.floor(timer / HOUR_IN_MINUTES);
   const minutes = timer % HOUR_IN_MINUTES;
 
-  const restoreCountdown = () => {
-    if (timer === 0) {
+  const restoreCountdown = useCallback(() => {
+    if (timer === 0 && currentIteration <= totalIterations) {
       setIsCounting(false);
       setTimer(DEFAULT_TIMER_VALUE);
+      console.log(currentIteration);
+      completeIterations();
     }
 
     clearInterval(intervalRef.current);
-  };
+  }, [currentIteration, timer, completeIterations, totalIterations]);
 
   const startCountdown = () => {
     setIsCounting(true);
@@ -27,15 +33,14 @@ export const useCountdown = () => {
     const DELAY = 1000;
     intervalRef.current = setInterval(() => {
       if (intervalRef.current) setTimer((old) => old - 1);
-    }, DELAY);
+    }, DELAY * 60);
   };
 
   useEffect(() => {
     if (timer === 0) {
-      clearInterval(intervalRef.current);
-      return;
+      restoreCountdown();
     }
-  }, [timer]);
+  }, [restoreCountdown, timer]);
 
   return {
     timer: { hour, minutes },
